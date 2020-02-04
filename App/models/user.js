bde1318dc68c6da47c0d448e6245ffa13118ddf7
LaupWing <loc_strict@hotmail.com>
2020-02-04 14:17:11 +0100

    added user model

---
+++ App/models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age:{
+        type: Number,
+        required: true,
+        validate(value){
+            if(value<18){
+                throw new Error('You are to young to be using an dating app')
+            }
+        }
+    },
+    password:{
+        type: String,
+        trim: true,
+        minlength: 6,
+        required: true,
+        validate(value){
+            const hasNumber = /\d/;
+            if(!hasNumber.test(value)){
+                throw new Error('Password has to contain at least one number bleep bleep')
+            }
+        }
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
