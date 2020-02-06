d10c6d1ba87abe26952ac51f738baee502a6580f
LaupWing <loc_strict@hotmail.com>
2020-02-06 20:41:10 +0100

    finally the right data is exported

--- App/routes/utils/filterByNeeds.js
+++ App/routes/utils/filterByNeeds.js
@@ -2,6 +2,6 @@ const User = require('../../models/user')
 
 module.exports = async (req)=>{
     const dbFiltered = await User.find({})
-        .where('minAge').gt(req.user.minAge).lt(req.user.maxAge)
-    return dbFiltered.filter(user=>!req.user.seen.some(seen=>seen.userId===user._id))
+        .where('age').gt(req.user.minAge).lt(req.user.maxAge)
+    return dbFiltered.filter(user=>!req.user.seen.some(seen=>seen.userId===user._id) && req.user._id !== user._id)
 }
