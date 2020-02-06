d5f71b715f5ae8d95a6b573954c8a658a1ed4dae
LaupWing <loc_strict@hotmail.com>
2020-02-06 23:16:12 +0100

    added both ways filtering

--- App/routes/utils/filterByNeeds.js
+++ App/routes/utils/filterByNeeds.js
@@ -3,5 +3,7 @@ const User = require('../../models/user')
 module.exports = async (req)=>{
     const dbFiltered = await User.find({})
         .where('age').gt(req.user.minAge).lt(req.user.maxAge)
+        .where('minAge').lt(req.user.age)
+        .where('maxAge').gt(req.user.age)
     return dbFiltered.filter(user=>!req.user.seen.some(seen=>seen.userId.equals(user._id)) && req.user._id !== user._id)
 }
