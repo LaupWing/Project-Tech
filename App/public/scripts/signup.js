a8a96b25e38d220fe9353a0511270c37f17a4078
LaupWing <loc_strict@hotmail.com>
2020-02-08 02:16:20 +0100

    added transitions

--- App/public/scripts/signup.js
+++ App/public/scripts/signup.js
@@ -3,19 +3,47 @@ console.log('signuppage')
 // When user changes his/her age the slides automatic changes
 class imageLoader{
     constructor(){
-        this.input = document.querySelector('input[type="file"]') 
-        console.log(this.imageLoader)
+        this.input = document.querySelector('input[type="file"]')
         this.input.addEventListener('change',()=> this.readFile(this))
     }
     readFile(input){
         if(input.input.files && input.input.files[0]){
             const reader = new FileReader()
             reader.onload = function (e){
-                console.log(e)
                 document.querySelector('img').src = e.target.result
             }
             reader.readAsDataURL(input.input.files[0]);
         }
     }
 }
-new imageLoader()
+
+class nextSection{
+    constructor(){
+        this.nextBtn = document.querySelector('button.next')
+        this.backBtn = document.querySelector('button.back')
+        
+        this.nextBtn.addEventListener('click', this.next)
+        this.backBtn.addEventListener('click', this.back)
+    }
+    next(){
+        const done = document.querySelectorAll('.done')
+        const fields = document.querySelectorAll('.field')
+        const ended = ()=>{
+            fields[done.length+1].classList.add('visible')
+            fields[done.length].removeEventListener('transitionend', ended)
+        }
+
+        fields[done.length].addEventListener('transitionend', ended)
+        fields[done.length].classList.add('done')
+    }
+    back(){
+        console.log('back')
+    }
+}
+
+const init = ()=>{
+    new imageLoader()
+    new nextSection()
+}
+
+init()
