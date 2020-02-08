5989fa96f7da19a4d3b0efa86005db685773a3cd
LaupWing <loc_strict@hotmail.com>
2020-02-08 01:50:22 +0100

    update

--- App/public/scripts/signup.js
+++ App/public/scripts/signup.js
@@ -1,3 +1,21 @@
 console.log('signuppage')
 // Todo!!!!!!!!!!!!!
 // When user changes his/her age the slides automatic changes
+class imageLoader{
+    constructor(){
+        this.input = document.querySelector('input[type="file"]') 
+        console.log(this.imageLoader)
+        this.input.addEventListener('change',()=> this.readFile(this))
+    }
+    readFile(input){
+        if(input.input.files && input.input.files[0]){
+            const reader = new FileReader()
+            reader.onload = function (e){
+                console.log(e)
+                document.querySelector('img').src = e.target.result
+            }
+            reader.readAsDataURL(input.input.files[0]);
+        }
+    }
+}
+new imageLoader()
