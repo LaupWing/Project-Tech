c2ec6576747876512c0c4ba4d9483029547236b0
LaupWing <loc_strict@hotmail.com>
2020-02-08 21:29:49 +0100

    update

--- App/public/scripts/app.js
+++ App/public/scripts/app.js
@@ -78,14 +78,31 @@ class showDetails{
     constructor(){
         socket.on('user detail', this.gotUserDetail.bind(this))
         this.mainNav = document.querySelector('.main-nav')
+        this.panels = document.querySelectorAll('.card-container > div')
+        this.detailPanel = document.querySelector('#detail')
     }
     gotUserDetail(user){
-        console.log(user)
-        this.setActiveLink()
+        this.setActiveLinkAndPanel()
+        this.renderDetail(user)
     }
-    setActiveLink(){
+    setActiveLinkAndPanel(){
         this.mainNav.querySelectorAll('li').forEach(li=>li.classList.remove('active'))
         this.mainNav.querySelector('.info').classList.add('active')
+        
+        this.panels.forEach(panel=>panel.classList.remove('hidden'))
+        document.querySelector('#card').classList.add('hidden')
+    }
+    renderDetail(user){
+        const h2 = this.detailPanel.querySelector('h2')
+        const gender = this.detailPanel.querySelector('.gender')
+        const age = this.detailPanel.querySelector('.age')
+        const img = this.detailPanel.querySelector('img')
+        
+        img.src = user.images.find(img=>img.mainPicture).url
+        h2.textContent = user.name
+        age.textContent = user.age
+        gender.textContent = user.gender
+        
     }
 }
 
