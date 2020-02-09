052c83f6822e740d48c6a9ae9a8a7084a2d4dd6d
LaupWing <loc_strict@hotmail.com>
2020-02-09 12:30:08 +0100

    getMatches is now Matches and moved to seperate file

--- App/public/scripts/app-parts/Matches.js
+++ App/public/scripts/app-parts/Matches.js
@@ -1,13 +1,12 @@
-export default class getMatch{
+export default class Matches{
     constructor(){
-        socket.removeAllListeners()
-        socket.emit('get match')
-        socket.on('sending match', this.renderMatch.bind(this))
         this.card = document.getElementById("matching");
+        console.log(this.card)
         this.card.querySelector('button.yes').addEventListener('click', this.accepted.bind(this))
         this.card.querySelector('button.no').addEventListener('click', this.denied.bind(this))
     }
     renderMatch(person){
+        console.log(this)
         const h2 = this.card.querySelector('h2')
         const age = this.card.querySelector('.age')
         const gender = this.card.querySelector('.gender')
--- App/public/scripts/app.js
+++ App/public/scripts/app.js
@@ -1,32 +1,32 @@
 const socket = io()
-
-class getMatch{
-    constructor(){
-        socket.removeAllListeners()
-        socket.emit('get match')
-        socket.on('sending match', this.renderMatch.bind(this))
-        this.card = document.getElementById("matching");
-        this.card.querySelector('button.yes').addEventListener('click', this.accepted.bind(this))
-        this.card.querySelector('button.no').addEventListener('click', this.denied.bind(this))
-    }
-    renderMatch(person){
-        const h2 = this.card.querySelector('h2')
-        const age = this.card.querySelector('.age')
-        const gender = this.card.querySelector('.gender')
-        h2.textContent = person.name
-        age.textContent = person.age
-        gender.textContent = person.gender
-        this.card.style.setProperty('--profile',`url(${person.images.find(i=>i.mainPicture).url})`)
-    }
-    denied(){
-        if(this.card.querySelector('.age').textContent === 'infinite')  return alert('FOR MY OWN')
-        socket.emit('denied match')
-    }
-    accepted(){
-        if(this.card.querySelector('.age').textContent === 'infinite')  return alert('FOR MY OWN')
-        socket.emit('accepted match')
-    }
-}
+import Matches from './app-parts/Matches.js'
+// class getMatch{
+//     constructor(){
+//         socket.removeAllListeners()
+//         socket.emit('get match')
+//         socket.on('sending match', this.renderMatch.bind(this))
+//         this.card = document.getElementById("matching");
+//         this.card.querySelector('button.yes').addEventListener('click', this.accepted.bind(this))
+//         this.card.querySelector('button.no').addEventListener('click', this.denied.bind(this))
+//     }
+//     renderMatch(person){
+//         const h2 = this.card.querySelector('h2')
+//         const age = this.card.querySelector('.age')
+//         const gender = this.card.querySelector('.gender')
+//         h2.textContent = person.name
+//         age.textContent = person.age
+//         gender.textContent = person.gender
+//         this.card.style.setProperty('--profile',`url(${person.images.find(i=>i.mainPicture).url})`)
+//     }
+//     denied(){
+//         if(this.card.querySelector('.age').textContent === 'infinite')  return alert('FOR MY OWN')
+//         socket.emit('denied match')
+//     }
+//     accepted(){
+//         if(this.card.querySelector('.age').textContent === 'infinite')  return alert('FOR MY OWN')
+//         socket.emit('accepted match')
+//     }
+// }
 class checkMatches{
     constructor(){
         socket.on('send matchesList', this.renderList.bind(this))
@@ -131,10 +131,14 @@ class switchPanel{
 }
 
 const init = ()=>{
-    new getMatch()
+    const matches = new Matches()
     new checkMatches()
     new showDetails()
     new switchPanel()
+
+    // Sockets
+    socket.emit('get match')
+    socket.on('sending match', matches.renderMatch.bind(matches))
 }
 
 init()
