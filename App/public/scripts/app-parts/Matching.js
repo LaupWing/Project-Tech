1117798cd44b9be38c11171fc636a4af91f2b401
LaupWing <loc_strict@hotmail.com>
2020-02-09 16:01:29 +0100

    added base for swipes

--- App/public/scripts/app-parts/Matching.js
+++ App/public/scripts/app-parts/Matching.js
@@ -1,37 +1,56 @@
 export default class Matches{
     constructor(socket){
-        this.card = document.getElementById("matching")
-        this.card.querySelector('button.yes').addEventListener('click', this.accepted.bind(this))
-        this.card.querySelector('button.no').addEventListener('click', this.denied.bind(this))
+        this.matching = document.getElementById("matching")
+        this.matching.querySelector('button.yes').addEventListener('click', this.accepted.bind(this))
+        this.matching.querySelector('button.no').addEventListener('click', this.denied.bind(this))
         this.socket = socket
+        this.init = true
+        this.action = null
     }
     renderMatch(person){
-        const h2 = this.card.querySelector('h2')
-        const age = this.card.querySelector('.age')
-        const gender = this.card.querySelector('.gender')
+        const h2 = this.matching.querySelector('h2')
+        const age = this.matching.querySelector('.age')
+        const gender = this.matching.querySelector('.gender')
+
         h2.textContent = person.name
         age.textContent = person.age
         gender.textContent = person.gender
-        this.card.style.setProperty('--profile',`url(${person.images.find(i=>i.mainPicture).url})`)
+        this.matching.style.setProperty('--profile',`url(${person.images.find(i=>i.mainPicture).url})`)
+        console.log(this.init)
+        if(!this.init){
+            const transitionEnded = ()=>{
+                this.matching.style.removeProperty('transitionDelay')
+                this.matching.style.removeProperty('transition')
+                this.matching.removeEventListener('transitionend', transitionEnded)
+            }
+            this.matching.addEventListener('transitionend', transitionEnded)
+            this.matching.style.transition = '1s opacity'
+            this.matching.style.transitionDelay = '.2s'
+            this.matching.classList.remove('accepted')
+            this.matching.classList.remove('denied')
+        }else{
+            this.init = false
+        }
     }
     denied(){
-        if(this.card.querySelector('.age').textContent === 'infinite')  return alert('FOR MY OWN')
-        // this.socket.emit('denied match')
-        this.card.classList.add('accepted')
+        if(this.matching.querySelector('.age').textContent === 'infinite')  return alert('FOR MY OWN')
+        this.matching.classList.add('denied')
+        this.matching.addEventListener('transitionend', this.showNewMatch.bind(this))
     }
     accepted(){
-        if(this.card.querySelector('.age').textContent === 'infinite')  return alert('FOR MY OWN')
-        // this.socket.emit('accepted match')
-        this.card.classList.add('accepted')
-        this.card.addEventListener('transitionend', this.showNewMatch.bind(this))
+        if(this.matching.querySelector('.age').textContent === 'infinite')  return alert('FOR MY OWN')
+        this.matching.classList.add('accepted')
+        this.matching.addEventListener('transitionend', this.showNewMatch.bind(this))
     }
     showNewMatch(e){
         if(e.propertyName === 'opacity'){
-            this.card.removeEventListener('transitionend', this.showNewMatch)
-            this.card.style.transition = '1s opacity'
-            this.card.style.transitionDelay = '.2s'
-            this.card.classList.remove('accepted')
-            this.card.classList.remove('denied')
+            this.matching.removeEventListener('transitionend', this.showNewMatch)
+            console.log(e.target.classList[0])
+            if(e.target.classList[0]==='accepted'){
+                this.socket.emit('accepted match')
+            }else if(e.target.classList[0]==='denied'){
+                this.socket.emit('denied match')
+            }
         }
     }
 }
