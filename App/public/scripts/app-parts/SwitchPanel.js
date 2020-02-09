ed864273f5dd90791eff15b918b9aa14c490081e
LaupWing <loc_strict@hotmail.com>
2020-02-09 12:59:26 +0100

    moved everything to a seperate file

---
+++ App/public/scripts/app-parts/SwitchPanel.js
@@ -0,0 +1,24 @@
+class SwitchPanel{
+    constructor(){
+        this.mainNav = document.querySelector('.main-nav')
+        this.cardContainer = document.querySelector('.card-container')
+        this.panels = document.querySelectorAll('.card-container > div')
+
+        this.items = this.mainNav.querySelectorAll('li')
+        this.items.forEach(item=>item.addEventListener('click', this.switch.bind(this)))
+    }
+
+    switch(event){
+        const panel = event.target.classList[0]
+
+        this.items.forEach(item=>item.classList.remove('active'))
+        event.target.classList.add('active')
+        this.panels.forEach(p=>{
+            if(p.id !== panel){
+                p.classList.add('hidden')
+            }else{
+                p.classList.remove('hidden')
+            }
+        })
+    }
+}
--- App/public/scripts/app.js
+++ App/public/scripts/app.js
@@ -2,37 +2,13 @@ const socket = io()
 import Matching from './app-parts/Matching.js'
 import MatchesList from './app-parts/MatchesList.js'
 import Details from './app-parts/Details.js'
-
-class switchPanel{
-    constructor(){
-        this.mainNav = document.querySelector('.main-nav')
-        this.cardContainer = document.querySelector('.card-container')
-        this.panels = document.querySelectorAll('.card-container > div')
-
-        this.items = this.mainNav.querySelectorAll('li')
-        this.items.forEach(item=>item.addEventListener('click', this.switch.bind(this)))
-    }
-
-    switch(event){
-        const panel = event.target.classList[0]
-
-        this.items.forEach(item=>item.classList.remove('active'))
-        event.target.classList.add('active')
-        this.panels.forEach(p=>{
-            if(p.id !== panel){
-                p.classList.add('hidden')
-            }else{
-                p.classList.remove('hidden')
-            }
-        })
-    }
-}
+import SwitchPanel from './app-parts/SwitchPanel.js'
 
 const init = ()=>{
     const matches = new Matching()
     const matchesList = new MatchesList((e)=>socket.emit('show detail', e.target.id))
     const details = new Details()
-    new switchPanel()
+    new SwitchPanel()
 
     // Sockets
     socket.emit('get match')
