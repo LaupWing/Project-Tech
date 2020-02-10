70da6465b8a2a6bfc9f9120e5d3f2f58e7cec919
LaupWing <loc_strict@hotmail.com>
2020-02-10 23:11:17 +0100

    added switchmenu

--- App/public/scripts/app-parts/MatchesList.js
+++ App/public/scripts/app-parts/MatchesList.js
@@ -1,6 +1,6 @@
 export default class MatchesList{
     constructor(eventFunction){
-        this.matchesList = document.querySelector('.match-list')
+        this.matchesList = document.querySelector('.active-list .match-list')
         this.totalNewmatches = document.querySelector('.newMatches')
         this.cb = eventFunction
     }
--- App/public/scripts/app-parts/SwitchMenu.js
+++ App/public/scripts/app-parts/SwitchMenu.js
@@ -1,12 +1,13 @@
 export default class SwitchMenu{
     constructor(){
-        this.messagesList = document.querySelector('.menu .messagesList')
-        this.matchesList = document.querySelector('.menu .matchesList')
-
-        this.messagesList.addEventListener('click', this.switchMenu)
-        this.matchesList.addEventListener('click', this.switchMenu)
+        this.messagesList = document.querySelector('.menu .message-list')
+        this.matchesList = document.querySelector('.menu .match-list')
+        this.menus = document.querySelectorAll('.active-list > div')
+        this.messagesList.addEventListener('click', this.switchMenu.bind(this))
+        this.matchesList.addEventListener('click', this.switchMenu.bind(this))
     }
-    switchMenu(){
-        console.log(this)
+    switchMenu(e){
+        const active = e.target.classList
+        console.log(active)
     }
 } 
