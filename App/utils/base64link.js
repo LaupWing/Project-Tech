0718581011641653381d3c9b2693c1f0f438c62a
LaupWing <loc_strict@hotmail.com>
2020-02-11 12:48:20 +0100

    dummy data creator worsk

---
+++ App/utils/base64link.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch')
+const FormData = require('form-data')
+
+const imgurBase64Upload = async(base64)=>{
+    const url = 'https://api.imgur.com/3/image'
+    const form = new FormData()
+    const cliendId = '8a49497fbb1b7c4'
+    form.append('image', base64)
+    const options = {
+        method: 'POST',
+        body: form,
+        headers:{
+            Authorization: `Client-ID ${cliendId}`
+        }
+    }
+    const res = await fetch(url,options)
+    const json = await res.json()
+    return json.data.link
+}
+
+module.exports = imgurBase64Upload
--- dummydata/createDummyData.js
+++ dummydata/createDummyData.js
@@ -1,16 +1,9 @@
 const User = require('../App/models/user')
 const fs = require('fs')
 const path = require('path')
-const fetch = require('node-fetch')
-const FormData = require('form-data')
-//     email,
-//     password,
-//     passwordCheck,
-//     age,
-//     name,
-//     minAge,
-//     gender,
-//     maxAg
+const imgurBase64Upload = require('../App/utils/base64link')
+require('../App/db/mongoose')
+
 const users = [
     {
         image: '1.jpeg',
@@ -22,128 +15,111 @@ const users = [
         maxAge: 50,
         gender: 'male'
     },
-    // {
-    //     image: '2.jpg',
-    //     name: 'Kees',
-    //     email: 'kees@hotmail.com',
-    //     password: 'test123',
-    //     age: 40,
-    //     minAge: 18,
-    //     maxAge: 60,
-    //     gender: 'male'
-    // },
-    // {
-    //     image: '3.jpeg',
-    //     name: 'Marjolein',
-    //     email: 'marjolein@hotmail.com',
-    //     password: 'test123',
-    //     age: 18,
-    //     minAge: 18,
-    //     maxAge: 50,
-    //     gender: 'female'
-    // },
-    // {
-    //     image: '4.jpg',
-    //     name: 'Henk',
-    //     email: 'henk@hotmail.com',
-    //     password: 'test123',
-    //     age: 45,
-    //     minAge: 30,
-    //     maxAge: 50,
-    //     gender: 'male'
-    // },
-    // {
-    //     image: '5.jpg',
-    //     name: 'Thierry',
-    //     email: 'thierry@hotmail.com',
-    //     password: 'test123',
-    //     age: 25,
-    //     minAge: 18,
-    //     maxAge: 35,
-    //     gender: 'male'
-    // },
-    // {
-    //     image: 'johny_bravo.png',
-    //     name: 'Johny Bravo',
-    //     email: 'johny@hotmail.com',
-    //     password: 'test123',
-    //     age: 22,
-    //     minAge: 18,
-    //     maxAge: 40,
-    //     gender: 'male'
-    // },
-    // {
-    //     image: 'lois.jpg',
-    //     name:'Lois',
-    //     email: 'lois@hotmail.com',
-    //     password: 'test123',
-    //     age: 38,
-    //     minAge: 22,
-    //     maxAge: 55,
-    //     gender: 'female'
-    // },
-    // {
-    //     image: 'malone.jpg',
-    //     name:'Toasty Malony',
-    //     email: 'toasty@hotmail.com',
-    //     password: 'test123',
-    //     age: 24,
-    //     minAge: 18,
-    //     maxAge: 30,
-    //     gender: 'male'
-    // },
-    // {
-    //     image: 'marge.jpg',
-    //     name: 'Marge Simpson',
-    //     email: 'marge@hotmail.com',
-    //     password: 'test123',
-    //     age: 45,
-    //     minAge: 30,
-    //     maxAge: 50,
-    //     gender: 'female'
-    // },
-    // {
-    //     image: 'misty.png',
-    //     name: 'Misty',
-    //     email: 'misty@hotmail.com',
-    //     password: 'test123',
-    //     age: 18,
-    //     minAge: 18,
-    //     maxAge: 50,
-    //     gender: 'female'
-    // },
+    {
+        image: '2.jpg',
+        name: 'Kees',
+        email: 'kees@hotmail.com',
+        password: 'test123',
+        age: 40,
+        minAge: 18,
+        maxAge: 60,
+        gender: 'male'
+    },
+    {
+        image: '3.jpeg',
+        name: 'Marjolein',
+        email: 'marjolein@hotmail.com',
+        password: 'test123',
+        age: 18,
+        minAge: 18,
+        maxAge: 50,
+        gender: 'female'
+    },
+    {
+        image: '4.jpg',
+        name: 'Henk',
+        email: 'henk@hotmail.com',
+        password: 'test123',
+        age: 45,
+        minAge: 30,
+        maxAge: 50,
+        gender: 'male'
+    },
+    {
+        image: '5.jpg',
+        name: 'Thierry',
+        email: 'thierry@hotmail.com',
+        password: 'test123',
+        age: 25,
+        minAge: 18,
+        maxAge: 35,
+        gender: 'male'
+    },
+    {
+        image: 'johny_bravo.png',
+        name: 'Johny Bravo',
+        email: 'johny@hotmail.com',
+        password: 'test123',
+        age: 22,
+        minAge: 18,
+        maxAge: 40,
+        gender: 'male'
+    },
+    {
+        image: 'lois.jpg',
+        name:'Lois',
+        email: 'lois@hotmail.com',
+        password: 'test123',
+        age: 38,
+        minAge: 22,
+        maxAge: 55,
+        gender: 'female'
+    },
+    {
+        image: 'malone.jpg',
+        name:'Toasty Malony',
+        email: 'toasty@hotmail.com',
+        password: 'test123',
+        age: 24,
+        minAge: 18,
+        maxAge: 30,
+        gender: 'male'
+    },
+    {
+        image: 'marge.jpg',
+        name: 'Marge Simpson',
+        email: 'marge@hotmail.com',
+        password: 'test123',
+        age: 45,
+        minAge: 30,
+        maxAge: 50,
+        gender: 'female'
+    },
+    {
+        image: 'misty.png',
+        name: 'Misty',
+        email: 'misty@hotmail.com',
+        password: 'test123',
+        age: 18,
+        minAge: 18,
+        maxAge: 50,
+        gender: 'female'
+    },
 ]
 
-const imgurBase64Upload = async(base64)=>{
-    console.log('upload..')
-    const url = 'https://api.imgur.com/3/image'
-    const form = new FormData()
-    const cliendId = '8a49497fbb1b7c4'
-    form.append('image', base64)
-    const options = {
-        method: 'POST',
-        body: form,
-        headers:{
-            Authorization: `Client-ID ${cliendId}`
-        }
-    }
-    const res = await fetch(url,options)
-    const json = await res.json()
-    
-    return json.data.link
-}
+
 const savingUsers = users.map(async (user)=>{
     const file = fs.readFileSync(path.join(__dirname,`images/${user.image}`))
     const base64data = new Buffer.from(file).toString('base64')
     let image=null
-    const res = await imgurBase64Upload(base64data)
-    console.log(res)
+    
     try{
-        image = res.data.link
+        const link = await imgurBase64Upload(base64data)
+        image = link
     }catch(e){
         image = base64data
     }
-    // console.log(image)
+    console.log(image)
     const newUser = new User({
         email: user.email,
         password: user.password,
@@ -164,7 +140,7 @@ const savingUsers = users.map(async (user)=>{
     }catch(e){
         console.log(e)
     }
-    // return newUser.save()
+    return newUser
     
 })
 
@@ -172,4 +148,4 @@ const savingUsers = users.map(async (user)=>{
 Promise.all(savingUsers).then(users=>{
         console.log('heh')
         console.log(users)
-    })
+})
