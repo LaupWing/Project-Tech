9ae24df41d4c07e5552a97c3d9e07813214c56f6
LaupWing <loc_strict@hotmail.com>
2020-02-12 12:24:56 +0100

    finalize user file

--- App/routes/app/users.js
+++ App/routes/app/users.js
@@ -1,4 +1,20 @@
 const activeUsers ={}
+const filterByNeeds = require('../utils/filterByNeeds')
+
+const setActiveUser=  async(socket, req)=>{
+    if(!activeUsers[`user_${socket.id}`]){
+        const filterForUser = await filterByNeeds(req)
+        activeUsers[`user_${socket.id}`] ={
+            couldBeAMatch: filterForUser,
+            currentMatching: null,
+            matchedUsers: null
+        }
+    }
+}
+
+const updateCouldBeAMatch = async(socket, req)=>{
+    activeUsers[`user_${socket.id}`].couldBeAMatch = await filterByNeeds(req)
+}
 
 const deleteUser = (socket)=>{
     delete activeUsers[`user_${socket.id}`]
@@ -6,5 +22,7 @@ const deleteUser = (socket)=>{
 
 module.exports = {
     activeUsers,
-    deleteUser
+    deleteUser,
+    setActiveUser,
+    updateCouldBeAMatch
 }
