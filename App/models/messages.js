9cb66341f3e390684b424661dbb76cf9eda932ff
LaupWing <loc_strict@hotmail.com>
2020-02-12 20:53:04 +0100

    findroom works

--- App/models/messages.js
+++ App/models/messages.js
@@ -2,21 +2,22 @@ const mongoose = require('mongoose')
 
 const messagesSchema = new mongoose.Schema({
     chatRoom:[{
-        userId:{
-            _id: false,
-            type: mongoose.Schema.Types.ObjectId
-        }
+        type: mongoose.Schema.Types.ObjectId
     }],
     messages:[{
+        _id: false,
         message: {
+            _id: false,
             type: String,
             required: true
         },
         date:{
+            _id: false,
             type: Date,
             required: true
         },
         read:{
+            _id: false,
             type:Boolean,
             default: false,
             required:true
--- App/routes/app/socketMessages.js
+++ App/routes/app/socketMessages.js
@@ -1,10 +1,16 @@
 const Messages = require('../../models/messages')
 const {activeUsers} = require('./users')
 
-const checkMessages = (id, socket, req)=>{
+const checkMessages = async (id, socket, req)=>{
     const findUser = activeUsers[`user_${socket.id}`].matchedUsers.find(u=>u.id === id)
-    console.log(findUser)
-    console.log(id)
+    const findRoom = await Messages.findOne({ chatRoom: { $all: [req.user._id, findUser.userId] } })
+    
+    if(!findRoom){
+        const newRoom = new Messages({
+            chatRoom:[req.user._id, findUser.userId] 
+        })
+        await newRoom.save()
+    }
 }
 
 module.exports = {
