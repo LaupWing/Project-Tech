70c83d781bea7ab03c8f336f717cb18b7b607f48
LaupWing <loc_strict@hotmail.com>
2020-02-26 19:37:36 +0100

    added progressbar functional

--- App/public/scripts/signup-parts/progress-bar.js
+++ App/public/scripts/signup-parts/progress-bar.js
@@ -1,8 +1,10 @@
-class ProgressBar{
+export default class ProgressBar{
     constructor(){
-        this._bars   = document.querySelectorAll('.bar')
-        this._fields = Array.from(document.querySelectorAll('.field'))
+        this._bars    = document.querySelectorAll('.bar')
+        this._fields  = Array.from(document.querySelectorAll('.field'))
+        this._buttons = document.querySelectorAll('form button')
         this.checkDone()
+        this._fields.forEach(field=>field.addEventListener('transitionend', this.checkDone.bind(this)))
     }
     checkDone(){
         const done = this._fields.filter(field=>field.classList.contains('done')).length
