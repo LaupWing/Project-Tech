12a14e098ca6b8bb04a6f465e875d004c227759d
LaupWing <loc_strict@hotmail.com>
2020-02-29 14:51:33 +0100

    added create to tyri it out

--- App/public/scripts/app-parts/utils/component.js
+++ App/public/scripts/app-parts/utils/component.js
@@ -1,4 +1,20 @@
 export default class {
+    constructor(){
+        this.el
+    }
+    create(element){
+        const checkIdOrClass = element.includes('#') || element.includes('.') 
+        if(checkIdOrClass){
+            const splitted = element.includes('#') ? element.split('#') : element.split('.')
+            this.el = document.createElement(splitted[0])
+            if(splitted[1]=== '#'){
+                this.el.id = splitted[1].replace('#', '')
+            }else{
+                this.el.className = splitted[1].replace('.', '')
+            }
+        }
+        return this
+    }
     removeChilds(parent){
         while (parent.firstChild) {
             parent.firstChild.remove()
