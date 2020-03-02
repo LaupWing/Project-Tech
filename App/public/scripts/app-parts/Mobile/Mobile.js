9b23abf52416fefe43e5a3e79a6bc4be7266f885
LaupWing <loc_strict@hotmail.com>
2020-03-02 22:24:47 +0100

    addd mobile js functional

--- App/public/scripts/app-parts/Mobile/Mobile.js
+++ App/public/scripts/app-parts/Mobile/Mobile.js
@@ -5,13 +5,32 @@ export default class extends Component{
     constructor(){
         super()
         this.body = document.body
+        this.nav  = document.querySelector('nav') 
+        this.menu
         this.renderMenu()
+        this.nav.classList.add('hide')
+        setTimeout(()=>{
+            this.nav.style.transition = 'all 1s'
+        })
     }
+    
     renderMenu(){
         const mobileMenu = this.create('div.menu-mobile').el
         const backdrop   = this.create('div.backdrop').el
 
         this.insertRawHTMLText(mobileMenu, [Hearth, '<h2>Menu</h2>', Chat])
         this.appendChilds(this.body, [mobileMenu, backdrop])
+
+        this.menu = mobileMenu
+        console.log(this.menu)
+        this.menu.querySelector('h2').addEventListener('click', this.openMenu.bind(this))
+    }
+    openMenu(){
+        this.nav.classList.toggle('hide')
+        if(this.nav.classList.contains('hide')){
+            this.menu.querySelector('h2').classList.remove('active')
+        }else{
+            this.menu.querySelector('h2').classList.add('active')
+        }
     }
 }
