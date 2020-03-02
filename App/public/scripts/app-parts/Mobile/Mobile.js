9431c1c23d28924aee2762318927a4a74cb7555e
LaupWing <loc_strict@hotmail.com>
2020-03-02 14:06:30 +0100

    addedchat

--- App/public/scripts/app-parts/Mobile/Mobile.js
+++ App/public/scripts/app-parts/Mobile/Mobile.js
@@ -1,5 +1,7 @@
 import Component from '../utils/component.js'
 import Logo from '../SVGstrings/logo.js'
+import Hearth from '../SVGstrings/hearth.js'
+import Chat from '../SVGstrings/chat.js'
 export default class extends Component{
     constructor(){
         super()
@@ -7,14 +9,10 @@ export default class extends Component{
         this.renderMenu()
     }
     renderMenu(){
-        
         const mobileMenu = this.create('div.menu-mobile').el
-        const bar1       = this.create('div.bar1').el
-        const bar2       = this.create('div.bar2').el
-        const bar3       = this.create('div.bar3').el
         const backdrop   = this.create('div.backdrop').el
 
-        this.appendChilds(mobileMenu, [bar1, bar2, bar3])
+        this.insertRawHTMLText(mobileMenu, [Hearth, Logo, Chat])
         this.appendChilds(this.body, [mobileMenu, backdrop])
     }
 }
