352910b45fdb3c25a2a72e350445ca3a9d588adf
LaupWing <loc_strict@hotmail.com>
2020-03-06 10:34:28 +0100

    assigned click bar event

--- App/public/scripts/signup-parts/progress-bar.js
+++ App/public/scripts/signup-parts/progress-bar.js
@@ -3,10 +3,15 @@ export default class ProgressBar{
         this._bars    = document.querySelectorAll('.bar')
         this._fields  = Array.from(document.querySelectorAll('.field'))
         this._buttons = document.querySelectorAll('form button')
-        this.checkDone()
-        this._fields.forEach(field=>field.addEventListener('transitionend', this.checkDone.bind(this)))
+        this._checkDone()
+        this._fields.forEach(field=>field.addEventListener('transitionend', this._checkDone.bind(this)))
+        this._bars.forEach(bar=>bar.addEventListener('click', this._barClick.bind(this)))
     }
-    checkDone(){
+    _barClick(e){
+        const index = Array.from(this._bars).findIndex(el=>el===e.currentTarget)
+        console.log(e.currentTarget, index)
+    }
+    _checkDone(){
         const done = this._fields.filter(field=>field.classList.contains('done')).length
         this._bars.forEach(bar=>bar.classList.remove('active'))
         this._bars[done].classList.add('active')
