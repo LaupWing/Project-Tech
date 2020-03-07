f6a9df0aa0c43db94583ec80bda548bce778f5b2
LaupWing <loc_strict@hotmail.com>
2020-03-07 10:44:24 +0100

    moved scokets

---
+++ App/routes/app/sockets.js
@@ -0,0 +1,52 @@
+const {
+    getMatch,
+    acceptedMatch,
+    deniedMatch,
+    getUserDetail,
+    sendMatches
+} = require('./socketMatching')
+const {
+    setActiveUser,
+    deleteUser
+} = require('./users')
+const {
+    checkMessages,
+    initializeMessages,
+    openChat,
+    saveMsg
+} = require('./socketMessages')
+
+exports.sockets = (req,res)=>{
+    const io = req.app.get('socketio')
+        
+    io.on('connection', async (socket)=>{
+        console.log('connected', socket.id)
+        await setActiveUser(socket, req)
+        await sendMatches(socket, req)
+        await getMatch(socket)
+        await initializeMessages(socket, req)
+        
+        // !!!!!!!!!!!!!!! Need realtime update to
+        // ---Matches---
+        socket.on('show detail',    (id)=>     getUserDetail(id, socket, req))
+
+        socket.on('denied match',   ()=>       deniedMatch(socket, req))
+        socket.on('accepted match', ()=>       acceptedMatch(socket, req))
+        
+        // ---Messages---
+        socket.on('check messages', (id)=>     checkMessages(id, socket, req))
+        socket.on('open chat',      (chatId)=> openChat(chatId, socket, req))
+        socket.on('send message',   (msgObj)=> saveMsg(msgObj, socket, req, io))
+
+        socket.on('disconnect',     ()=>{
+            socket.removeAllListeners('denied match')
+            socket.removeAllListeners('check messages')
+            socket.removeAllListeners('accepted match')
+            socket.removeAllListeners('show detail')
+            socket.removeAllListeners('open chat')
+            socket.removeAllListeners('send message')
+            io.removeAllListeners('connection')
+            deleteUser(socket)
+        })
+    })
+}
