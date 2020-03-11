e16713488cd8269e3b15037e809a6925c53dc864
LaupWing <loc_strict@hotmail.com>
2020-03-11 11:34:03 +0100

    added progress error done active functinoal

--- App/public/scripts/signup-parts/progress-bar.js
+++ App/public/scripts/signup-parts/progress-bar.js
@@ -5,7 +5,9 @@ export default class ProgressBar{
         this._fields  = Array.from(document.querySelectorAll('.field'))
         this._buttons = document.querySelectorAll('form button')
         checkDone(this._bars)
-        this._fields.forEach(field=>field.addEventListener('transitionend', checkDone.bind(this, this._bars)))
+        this._fields.forEach(field=>
+            field.addEventListener('transitionend', checkDone.bind(this, this._bars))
+        )
         this._bars.forEach(bar=>bar.addEventListener('click', this._barClick.bind(this)))
     }
     _barClick(e){
--- App/public/scripts/signup-parts/progress-hearth.js
+++ App/public/scripts/signup-parts/progress-hearth.js
@@ -1,11 +1,18 @@
-import Warning from './FieldCheck/ProgressWarning.js'
+import Warning     from './FieldCheck/ProgressWarning.js'
+import {checkDone} from './utils/utils.js'
 
 export default class ProgressHearth{
     constructor(){
         this.warning         = new Warning()
         this.stepsContainer  = document.querySelector('.progress')
         this.inputs          = document.querySelectorAll('input')
+        this._svgs           = document.querySelectorAll('.step svg')
         this.steps           = document.querySelectorAll('.step')
+        this._fields         = Array.from(document.querySelectorAll('.field'))
+        checkDone(this._svgs)
+        this._fields.forEach(field=>
+            field.addEventListener('transitionend', checkDone.bind(this, this._svgs))
+        )
         this.inputs.forEach(input=>input.addEventListener('input', this.checkInput.bind(this))) 
         this.steps.forEach(step=>step.addEventListener('click', this.checkWarning.bind(this))) 
         this._initValueChecks()
