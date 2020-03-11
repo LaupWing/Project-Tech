619933a0a4ae6fd298dce991fa621e5f5076b1e1
LaupWing <loc_strict@hotmail.com>
2020-03-11 17:20:42 +0100

    added progresschecker to progress bar

--- App/public/scripts/signup-parts/progress-bar.js
+++ App/public/scripts/signup-parts/progress-bar.js
@@ -1,16 +1,20 @@
 import {checkDone} from './utils/utils.js'
+import ProgressChecker from './utils/ProgressChecker.js'
 
 export default class ProgressBar{
     constructor(){
-        this._bars    = document.querySelectorAll('.bar')
-        this._fields  = Array.from(document.querySelectorAll('.field'))
-        this._buttons = document.querySelectorAll('form button')
-        this.inputs   = document.querySelectorAll('input')
+        this._bars     = document.querySelectorAll('.bar')
+        this._fields   = Array.from(document.querySelectorAll('.field'))
+        this._buttons  = document.querySelectorAll('form button')
+        this.inputs    = document.querySelectorAll('input')
+        this._progress = new ProgressChecker('mobile')
         checkDone(this._bars)
         this._fields.forEach(field=>
             field.addEventListener('transitionend', checkDone.bind(this, this._bars))
         )
-        this.inputs.forEach(input=>input.addEventListener('input', this.checkInput.bind(this))) 
+        this.inputs.forEach(input=>input.addEventListener('input', 
+            this._progress.checkInput.bind(this._progress))
+        ) 
         this._bars.forEach(bar=>bar.addEventListener('click', this._barClick.bind(this)))
     }
     _barClick(e){
