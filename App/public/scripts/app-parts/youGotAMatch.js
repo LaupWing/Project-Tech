ca709953c5ca8c29ee922ddbb9c55080a3c00e5e
LaupWing <loc_strict@hotmail.com>
2020-03-13 01:50:12 +0100

    working modal

--- App/public/scripts/app-parts/youGotAMatch.js
+++ App/public/scripts/app-parts/youGotAMatch.js
@@ -1,4 +1,6 @@
 export default (user)=>{
-    console.log(user)
-
+    const modal = document.querySelector('laup-modal')
+    modal.setAttribute('open', '')
+    modal.setAttribute('title', 'Its a match!')
+    modal.setAttribute('description', `You got a match with _${user.name}_`)
 }
