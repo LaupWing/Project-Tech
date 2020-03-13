8187ceffaf6460edc2e6a207c660e16e116ca0ca
LaupWing <loc_strict@hotmail.com>
2020-03-13 02:40:42 +0100

    progressWatcher update

--- App/public/scripts/signup-parts/FieldCheck/ProgressWarning.js
+++ App/public/scripts/signup-parts/FieldCheck/ProgressWarning.js
@@ -42,16 +42,18 @@ export default class ProgressWarning {
         return empty
     }
     _incomplete(){
-        this._modal.setAttribute('animation', 'fromTheTop')
-        this._modal.setAttribute('open', '')
-        this._modal.setAttribute('title', 'Incomplete')
-        this._modal.setAttribute('description', 'Please fill in _all_ fields')
+        this.setModal('Incomplete', 'Please fill in _all_ fields', 'fromTheTop')
     }
     _completed(){
-        this._modal.setAttribute('animation', 'fromTheTop')
-        this._modal.setAttribute('open', '')
-        this._modal.setAttribute('title', 'Complete')
-        this._modal.setAttribute('description', 'You have completed this field!')
+        this.setModal('Complete', 'You have completed this field!', 'fromTheTop')
+    }
+    setModal(title, description, animation){
+        this._modal.setAttribute('title', title)
+        this._modal.setAttribute('description', description)
+        this._modal.setAttribute('animation', animation)
+        setTimeout(()=>{
+            this._modal.setAttribute('open', '')
+        })
     }
     _inlogInfo(field){
         const empty = this._empty(field)
@@ -60,10 +62,7 @@ export default class ProgressWarning {
         const passwordCheck = field.querySelector('input[name="passwordCheck"]')
 
         if(password.value != passwordCheck.value){
-            this._modal.setAttribute('animation', 'fromTheTop')
-            this._modal.setAttribute('open', '')
-            this._modal.setAttribute('title', 'Password doesnt match')
-            this._modal.setAttribute('description', 'The filled in _passwords_ doesnt match with each other')
+            this.setModal('Password doesnt match', 'The filled in _passwords_ doesnt match with each other', 'fromTheTop')
             return
         }   
         if(empty){
@@ -95,10 +94,7 @@ export default class ProgressWarning {
         const maxAge = field.querySelector('input[name="maxAge"]')
         
         if(minAge.value > maxAge.value){
-            this._modal.setAttribute('animation', 'fromTheTop')
-            this._modal.setAttribute('open', '')
-            this._modal.setAttribute('title', 'Min Age Higher?')
-            this._modal.setAttribute('description', 'Your Min Age is higher than Max Age?? Min Age has to belower than Max Age')
+            this.setModal('Min Age Higher?',  'Your Min Age is higher than Max Age?? Min Age has to belower than Max Age', 'fromTheTop')
             return
         }  
         if(this._empty(field)){
