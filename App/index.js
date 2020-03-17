54d8ef5d12a7bbcfc050e37294edb64960c91c5f
LaupWing <loc_strict@hotmail.com>
2020-03-17 18:14:11 +0100

    added session

--- App/index.js
+++ App/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const path       = require('path')
 const http       = require('http')
 const socketio   = require('socket.io')
-
+const session    = require('express-session')
 // Own files
 require('./db/mongoose') // initialize the db connection 
 const authRoutes = require('./routes/auth')
@@ -19,6 +19,12 @@ const io     = socketio.listen(server)
 app
     .use(express.urlencoded({extended:false}))
     .use(cors())
+    .use(session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: true }
+      }))
     .use(bodyParser.json({limit: '50mb'}))
     .use('/static',express.static(path.join(__dirname, 'public')))
     .set('socketio', io)
